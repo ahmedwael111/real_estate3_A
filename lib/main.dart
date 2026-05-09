ab483e747180d67051dc3392abbfe7d9d69495f3
ahmed wael <ahmed855wael@gmail.com>
2026-05-09 07:39:51 +0300

    merge first branch'Salah Done.

--- lib/main.dart
+++ lib/main.dart
@@ -3,17 +3,15 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:real_estate3_a/core/bloc%20observe/bloc_service.dart';
 import 'package:real_estate3_a/core/di.dart';
-import 'package:real_estate3_a/features/Home/presentation/views/MainView.dart';
 import 'package:real_estate3_a/features/splash/presentation/views/splash_view.dart';
-
 import 'features/Home/domain/repo/HomeRepo.dart';
 import 'features/Home/presentation/home_cubit/home_cubit.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await initAppModule();
-  Bloc.observer=MyBlocObserver();
+  Bloc.observer = MyBlocObserver();
   runApp(const RealStateApp3A());
 }
 
@@ -27,18 +25,17 @@ class RealStateApp3A extends StatelessWidget {
       minTextAdapt: true,
       splitScreenMode: true,
       builder: (context, child) {
-          return BlocProvider(
+        return BlocProvider(
           create: (_) => HomeCubit(getIt<HomeRepository>()),
           child: MaterialApp(
-
             debugShowCheckedModeBanner: false,
             title: 'Real Estate 3A',
             theme: ThemeData(
               scaffoldBackgroundColor: Color(0xffFAF9F6),
               primarySwatch: Colors.blue,
             ),
-            home: const MainView(),
-          )
+            home: const SplashView(),
+          ),
         );
       },
     );
